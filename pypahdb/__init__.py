f74ddd53154a2a7c472c7a20f7537d53685890c2
Christiaan Boersma <Christiaan.Boersma@nasa.gov>
2018-03-20 11:52:13 -0700

    Added framework for reading in Spitzer-IRS data cubes and minor cosmetic fixes.

--- pypahdb/__init__.py
+++ pypahdb/__init__.py
@@ -1,8 +1,12 @@
 """
-The pyPAHdb module: Using a precomputed matrix of theoretically
-calculated PAH emission spectra from the NASA Ames PAH IR
-Spectroscopic Database a spectrum is decomposed into contribution PAH
-subclasses using a nnls-approach.
+The pyPAHdb package is being developed as part of the awarded James
+Webb Space Telescope (JWST) Early Release Science (ERS) program
+"Radiative Feedback from Massive Stars as Traced by Multiband Imaging
+and Spectroscopic Mosaics" (ID: 1288). The entire program is
+coordinated by an international "Core team" of 19 scientists and
+supported by 119 "science collaborators". pyPAHdb is developed by the
+NASA Ames PAH IR Spectroscopic Database team, asscociated with the
+Astrophysics & Astrochemistry Laboratory at NASA Ames Research Center.
 
 pyPAHdb uses a precomputed matrix of theoretically calculated PAH
 emission spectra from version 3.00 of the library of computed
@@ -26,19 +30,19 @@ www.astrochemistry.org/pahdb/.
 
 You are kindly asked to cite the following papers when using pyPAHdb:
 
+    * C.W. Bauschlicher, Jr., A. Ricca, C. Boersma, and
+      L.J. Allamandola, "THE NASA AMES PAH IR SPECTROSCOPIC DATABASE:
+      COMPUTATIONAL VERSION 3.00 WITH UPDATED CONTENT AND THE
+      INTRODUCTION OF MULTIPLE SCALING FACTORS", The Astrophysical
+      Journal Supplement Series, 234, 32, 2018
+      10.3847/1538-4365/aaa019
+
     * C. Boersma, C.W. Bauschlicher, Jr., A. Ricca, A.L. Mattioda,
       J. Cami, E. Peeters, F. Sanchez de Armas, G. Puerta Saborido,
       D.M. Hudgins, and L.J. Allamandola, "THE NASA AMES PAH IR
       SPECTROSCOPIC DATABASE VERSION 2.00: UPDATED CONTENT, WEBSITE AND
       ON/OFFLINE TOOLS", The Astrophysical Journal Supplement Series,
       211, 8, 2014 10.1088/0067-0049/211/1/8
-
-    * C.W. Bauschlicher, Jr., C. Boersma, A. Ricca, A.L. Mattioda,
-      J. Cami, E. Peeters, F. Sanchez de Armas, G. Puerta Saborido,
-      D.M. Hudgins, and L.J. Allamandola, "THE NASA AMES PAH IR
-      SPECTROSCOPIC DATABASE: THE COMPUTED SPECTRA", The Astrophysical
-      Journal Supplement Series, 189, 341, 2010
-      10.1088/0067-0049/189/2/341
 
     * Mattioda, A. L., Hudgins, D. M., Boersma, C., Ricca, A.,
       Peeters, E., Cami, J., Sanchez de Armas, F., Puerta Saborido,
