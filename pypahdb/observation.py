7c813e69e9e4e8f6409e4e287ddb9dff96c3bc3a
Matt Shannon <matthew.j.shannon@gmail.com>
2018-08-15 14:59:51 -0700

    Applied flake8 fixes to observation.py

--- pypahdb/observation.py
+++ pypahdb/observation.py
@@ -12,7 +12,6 @@ import numpy as np
 
 from astropy.io import ascii
 from astropy.io import fits
-from astropy import wcs
 
 from .spectrum import spectrum
 
@@ -37,29 +36,43 @@ class observation(object):
 
         try:
             with fits.open(self.file_path) as hdu:
-                # use the wcs definitions for coordinate three, either via linear scale or lookup table
-                if 'PS3_0' in hdu[0].header.keys() and 'PS3_1' in hdu[0].header.keys():
+                hdu_keys = hdu[0].header.keys()
+
+                # use the wcs definitions for coordinate three,
+                # either via linear scale or lookup table
+                if 'PS3_0' in hdu_keys and 'PS3_1' in hdu_keys:
                     self.header = hdu[0].header
-                    #self.wcs = wcs.WCS(hdu[0].header, naxis=2)
-                    self.spectrum = spectrum(hdu[self.header['PS3_0']].data[self.header['PS3_1']],
-                                             hdu[0].data,
-                                             np.zeros(hdu[0].data.shape),
-                                             {'abscissa':{'str':hdu[self.header['PS3_0']].columns[self.header['PS3_1']].name +  ' [' + hdu[self.header['PS3_0']].columns[self.header['PS3_1']].unit + ']'},
-                                              'ordinate':{'str':self.header['BUNIT']}})
+                    # self.wcs = wcs.WCS(hdu[0].header, naxis=2)
+                    h0 = self.header['PS3_0']
+                    h1 = self.header['PS3_1']
+                    abscissa_unit = hdu[h0].columns[h1].name + ' [' + \
+                        hdu[h0].columns[h1].unit + ']'
+                    ordinate_unit = self.header['BUNIT']
+
+                    # Create spectrum object.
+                    self.spectrum = \
+                        spectrum(hdu[h0].data[h1],
+                                 hdu[0].data,
+                                 np.zeros(hdu[0].data.shape),
+                                 {'abscissa': {'str': abscissa_unit},
+                                  'ordinate': {'str': ordinate_unit}})
                     return None
-        except:
+        except Exception as e:
+            print(e)
             pass
 
         try:
             data = ascii.read(self.file_path)
             self.header = fits.header.Header()
-            self.spectrum = spectrum(np.array(data[data.colnames[0]]),
-                                     np.array(data[data.colnames[1]]),
-                                     np.zeros(len(data[data.colnames[0]])),
-                                     {'abscissa':{'str':'wavelength [micron]'},
-                                      'ordinate':{'str':'surface brightness [MJy/sr]'}})
+            self.spectrum = \
+                spectrum(np.array(data[data.colnames[0]]),
+                         np.array(data[data.colnames[1]]),
+                         np.zeros(len(data[data.colnames[0]])),
+                         {'abscissa': {'str': 'wavelength [micron]'},
+                          'ordinate': {'str': 'surface brightness [MJy/sr]'}})
             return None
-        except:
+        except Exception as e:
+            print(e)
             pass
 
         raise IOError(self.file_path + ": File-format not recognized")
