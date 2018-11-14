ded90d6085fcc3c7972a5fdb20261c3778cc3f9c
Christiaan Boersma <Christiaan.Boersma@nasa.gov>
2018-11-14 12:28:34 -0800

    Added additional tests for decomposer.py and no longer including coverage for _version.py and tests themselves.

--- pypahdb/tests/test_decomposer.py
+++ pypahdb/tests/test_decomposer.py
@@ -6,59 +6,60 @@ test_spectrum.py: unit tests for class decomposer.
 """
 
 import unittest
-import pytest
-
+import os.path
 import numpy as np
 
 from pypahdb.observation import Observation
 from pypahdb.decomposer import Decomposer
 
 
-@pytest.fixture(scope='class', autouse=True)
-def observation_obj():
-    import pkg_resources
-    file_name = 'data/sample_data_NGC7023.dat'
-    file_path = pkg_resources.resource_filename('pypahdb', file_name)
-    return Observation(file_path)
-
-
 class DecomposerTestCase(unittest.TestCase):
     """Unit tests for `decomposer.py`"""
 
+    def setUp(self):
+        import pkg_resources
+        import tempfile
+        file_name = 'data/sample_data_NGC7023.dat'
+        file_path = pkg_resources.resource_filename('pypahdb', file_name)
+        self.observation = Observation(file_path)
+        self.decomposer = Decomposer(self.observation.spectrum)
+        self.tmpdir = tempfile.gettempdir()
+
     def test_is_instance(self):
         """Can we create an instance of Decomposer?"""
-        observation = observation_obj()
-        self.assertIsInstance(Decomposer(observation.spectrum), Decomposer)
+        self.assertIsInstance(self.decomposer, Decomposer)
 
     def test_has_fit(self):
         """Can we create a fit?"""
-        observation = observation_obj()
-        result = Decomposer(observation.spectrum)
-        self.assertIsInstance(result.fit, np.ndarray)
+        self.assertIsInstance(self.decomposer.fit, np.ndarray)
 
     def test_has_ionized_fraction(self):
         """Can we create an ionized fraction?"""
-        observation = observation_obj()
-        result = Decomposer(observation.spectrum)
-        self.assertIsInstance(result.ionized_fraction, np.ndarray)
+        self.assertIsInstance(self.decomposer.ionized_fraction, np.ndarray)
 
     def test_has_large_fraction(self):
         """Can we calculate a large fraction?"""
-        observation = observation_obj()
-        result = Decomposer(observation.spectrum)
-        self.assertIsInstance(result.large_fraction, np.ndarray)
+        self.assertIsInstance(self.decomposer.large_fraction, np.ndarray)
 
     def test_has_charge(self):
         """Can we generate a charge results?"""
-        observation = observation_obj()
-        result = Decomposer(observation.spectrum)
-        self.assertIsInstance(result.charge, dict)
+        self.assertIsInstance(self.decomposer.charge, dict)
 
     def test_has_size(self):
         """Can we generate a size results?"""
-        observation = observation_obj()
-        result = Decomposer(observation.spectrum)
-        self.assertIsInstance(result.size, dict)
+        self.assertIsInstance(self.decomposer.size, dict)
+
+    def test_do_pdf(self):
+        """Can we output a PDF?"""
+        ofile = os.path.join(self.tmpdir, "result.pdf")
+        self.decomposer.save_pdf(ofile)
+        self.assertTrue(os.path.isfile(ofile))
+
+    def test_do_fits(self):
+        """Can we output FITS?"""
+        ofile = os.path.join(self.tmpdir, "result.pdf")
+        self.decomposer.save_fits(ofile)
+        self.assertTrue(os.path.isfile(ofile))
 
 
 if __name__ == '__main__':
