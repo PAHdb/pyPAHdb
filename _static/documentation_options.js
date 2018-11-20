d8a994ab97f9d06d0f9f470a4c186678f7152bf9
Doctr (Travis CI) <drdoctr@users.noreply.github.com>
2018-11-20 20:50:34 +0000

    Update docs after building Travis build 42 of PAHdb/pyPAHdb
    The docs were built from the branch 'master' against the commit
    08f81a2394e0c6f781a6d0b4256876b1338c5cb0.
    
    The Travis build that generated this commit is at
    https://travis-ci.org/PAHdb/pyPAHdb/jobs/457641857.
    
    The doctr command that was run is
    
        /home/travis/virtualenv/python3.6.3/bin/doctr deploy --built-docs docs/build/html .

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.6.1.post13+g6aaa44d',
+    VERSION: '0.6.1.post14+g08f81a2',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     FILE_SUFFIX: '.html',
