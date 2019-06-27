2820c210851ee94318e58e5d6bd882d010192e92
Matt Shannon <matthew.j.shannon@gmail.com>
2019-06-27 16:40:48 -0700

    Unittest to pytest

--- pypahdb/tests/test_observation.py
+++ pypahdb/tests/test_observation.py
@@ -19,14 +19,14 @@ class SpectrumTestCase(unittest.TestCase):
         file_name = 'resources/sample_data_NGC7023.tbl'
         file_path = pkg_resources.resource_filename('pypahdb', file_name)
 
-        self.assertIsInstance(Observation(file_path), Observation)
+        assert isinstance(Observation(file_path), Observation)
 
     def test_read_fits(self):
         """Can we create an instance of Observation from a FITS file?"""
         file_name = 'resources/sample_data_NGC7023.fits'
         file_path = pkg_resources.resource_filename('pypahdb', file_name)
 
-        self.assertIsInstance(Observation(file_path), Observation)
+        assert isinstance(Observation(file_path), Observation)
 
 
 if __name__ == '__main__':
