ea3045758a27ebd6bab4d198c907fb74f2264552
PAHdb <Christiaan.Boersma@nasa.gov>
2021-05-28 14:43:21 -0700

    Have docs build again.

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -133,9 +133,11 @@ html_show_sourcelink = False
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
 
-
-def setup(app):
-    app.add_stylesheet('css/custom.css')
+# These paths are either relative to html_static_path
+# or fully qualified paths (eg. https://...)
+html_css_files = [
+    'css/custom.css',
+]
 
 # Custom sidebar templates, must be a dictionary that maps document names
 # to template names.
