66960c6e0e6eb1f4b87b460801ed45073b1fbc25
PAHdb <Christiaan.Boersma@nasa.gov>
2021-06-03 14:58:34 -0700

    Add Spectrum1D read test.

--- pypahdb/tests/test_observation.py
+++ pypahdb/tests/test_observation.py
@@ -14,6 +14,13 @@ from pypahdb.observation import Observation
 class SpectrumTestCase(unittest.TestCase):
     """Unit tests for `observation.py`"""
 
+    def test_read_spectrum1d(self):
+        """Can we create an instance of Observation from a Spectrum1D file?"""
+        file_name = 'resources/sample_data_jwst.fits'
+        file_path = pkg_resources.resource_filename('pypahdb', file_name)
+
+        assert isinstance(Observation(file_path), Observation)
+
     def test_read_ascii(self):
         """Can we create an instance of Observation from an ASCII file?"""
         file_name = 'resources/sample_data_NGC7023.tbl'
